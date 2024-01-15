1fd9aaf37e0e05bcf580c77feb9621e20e20385b
kayulan <kajalventhan30@gmail.com>
2024-01-16 00:08:26 +0530

    first commit

---
+++ calclutor.php
@@ -0,0 +1,26 @@
+<DOCTYPE html>
+<html>
+<title>
+
+</title>
+<head>
+</head>
+<body>
+   <form action="" method="">
+   first number:
+   <input name="number" value="<?php echo $txt1?>">
+   <br>
+   <br>
+   secound number:
+   <input name="number" value="<?php echo $txt1?>">
+   <br>
+   <br>
+   <input type="submit" name="cal" value="+">
+   <input type="submit" name="cal" value="-">
+   <input type="submit" name="cal" value="/">
+   <input type="submit" name="cal" value="*">
+   
+   </form>
+
+</body>
+</html>
